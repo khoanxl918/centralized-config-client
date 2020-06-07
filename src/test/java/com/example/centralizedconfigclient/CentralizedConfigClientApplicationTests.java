ee6816f362d8b97d648ecfab64adc62738156ce6
khoanxl918@gmail.com <khoanxl918@gmail.com>
2020-06-07 23:05:44 +0700

    first commit

---
+++ src/test/java/com/example/centralizedconfigclient/CentralizedConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centralizedconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
